c00b17738a5cedf756f74b672a5edebfa43ccba2
assiaSannaghi <assia.sannaghi>
2025-02-19 12:23:19 +0100

    Updates

---
+++ static/js/952.bb8324c1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshare_n_split=self.webpackChunkshare_n_split||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.bb8324c1.chunk.js.map
